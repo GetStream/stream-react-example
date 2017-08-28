1d2249c6662041f574236d12dc494b73ed1b49bb
Nick Parsons <nparsons08@gmail.com>
2017-08-28 11:37:57 -0600

    Upgraded npm modules. Styled with Prettify.

--- www/public/js/power.js
+++ www/public/js/power.js
@@ -1,13 +1,11 @@
 $(function() {
-
-    $('#scotch-panel').scotchPanel({
-        containerSelector: 'body',
-        direction: 'right',
-        duration: 300,
-        transition: 'ease',
-        clickSelector: '.toggle-panel',
-        distanceX: '70%',
-        enableEscapeKey: true
-    });
-
+	$('#scotch-panel').scotchPanel({
+		containerSelector: 'body',
+		direction: 'right',
+		duration: 300,
+		transition: 'ease',
+		clickSelector: '.toggle-panel',
+		distanceX: '70%',
+		enableEscapeKey: true,
+	});
 });
--- www/routes/index.js
+++ www/routes/index.js
@@ -1,41 +1,48 @@
-var express      = require('express'),
-    cors         = require('cors'),
-    router       = express.Router({'strict': true}),
-    config       = require('../config'),
-    MailChimpAPI = require('mailchimp').MailChimpAPI;
+var express = require('express'),
+	cors = require('cors'),
+	router = express.Router({ strict: true }),
+	config = require('../config'),
+	MailChimpAPI = require('mailchimp').MailChimpAPI;
 
 /* GET home */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Cabin: React Example App by Stream' });
+	res.render('index', { title: 'Cabin: React Example App by Stream' });
 });
 
 /* GET demo */
 router.get('/demo', function(req, res, next) {
-    res.render('demo', { title: 'Cabin: React Example App by Stream' });
+	res.render('demo', { title: 'Cabin: React Example App by Stream' });
 });
 
 router.get('/demo/', function(req, res, next) {
-    res.redirect(302, '/demo')
+	res.redirect(302, '/demo');
 });
 
 /* POST mailchimp */
-router.post('/subscribe', cors({ origin: 'https://getstream.io/blog' }), function(req, res, next) {
+router.post(
+	'/subscribe',
+	cors({ origin: 'https://getstream.io/blog' }),
+	function(req, res, next) {
+		var params = req.body;
 
-    var params = req.body;
+		var api = new MailChimpAPI(config.mailchimp.apiKey, { version: '2.0' });
 
-    var api = new MailChimpAPI(config.mailchimp.apiKey, { version : '2.0' });
-
-    api.call('lists', 'subscribe', {
-        id: config.mailchimp.listId,
-        email: {
-            email: params.email.toLowerCase()
-        },
-        double_optin: false,
-        send_welcome: false
-    }, function (err, data) {
-        res.sendStatus(200).end();
-    });
-
-});
+		api.call(
+			'lists',
+			'subscribe',
+			{
+				id: config.mailchimp.listId,
+				email: {
+					email: params.email.toLowerCase(),
+				},
+				double_optin: false,
+				send_welcome: false,
+			},
+			function(err, data) {
+				res.sendStatus(200).end();
+			},
+		);
+	},
+);
 
 module.exports = router;
